ff74ebe7d3f3de6e7b159f3355430b5d56a3d728
unknown <kabyldorado@hotmail.com>
2020-12-21 03:54:27 -0600

    adding the files to the repo

---
+++ client/src/components/background.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import WOW from "wowjs";
+
+export default class Background extends React.Component {
+	componentDidMount() {
+		new WOW.WOW().init();
+	}
+
+	render() {
+		return (
+			<div id="my-background" className="background">
+				<div id="stars" />
+				<div id="stars2" />
+				<div id="stars3" />
+			</div>
+		);
+	}
+}
+
+Background.propTypes = {
+};
