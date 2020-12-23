4fe13115a5f259185d8842a2b8ee6f7d36082ef1
unknown <kabyldorado@hotmail.com>
2020-12-22 20:52:36 -0600

    troubleshooting

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({  extended: false }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
